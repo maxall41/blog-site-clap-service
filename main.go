3d3ff5a8a192b20afd99db0f5a132a7e14b1c5df
maxall4 <maxall4@protonmail.com>
2022-03-29 12:53:48 -1000

    Init

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+var ctx = context.Background()
+
+func toNumberFromString(start string) int {
+    val, err := strconv.Atoi(start)
+    if err != nil {
+			panic(err)
+	}
+    return val
+}
+
+func main() {
+    app := fiber.New()
+    app.Use(cors.New())
+
+	
+	rdb := redis.NewClient(&redis.Options{
+        Addr: "redis-11387.c289.us-west-1-2.ec2.cloud.redislabs.com:11387",
+        Password: "SeMAUKONqjtqrO73wCK49O9DY6mGAsRG", // no password set
+        DB:       0,  // use default DB
+    })
+
+    app.Get("/clap/:article/:claps", func(c *fiber.Ctx) error {
+        val, err := rdb.Get(ctx, c.Params("article")).Result()
+        if err != nil {
+            if (err.Error() == "redis: nil") {
+                val = "0";
+            } else {
+                panic(err)
+            }
+        }
+		err2 := rdb.Set(ctx, c.Params("article"), toNumberFromString(val) + toNumberFromString(c.Params("claps")), 0).Err()
+		if err2 != nil {
+			panic(err2)
+		}
+        return c.SendString("👏")
+    })
+
+    app.Get("/get-claps/:article", func(c *fiber.Ctx) error {
+        val, err := rdb.Get(ctx, c.Params("article")).Result()
+        if err != nil {
+            if (err.Error() == "redis: nil") {
+                return c.SendString("0")
+            } else {
+                panic(err)
+            }
+        }
+        return c.SendString(val)
+    })
+
+    app.Listen(":3000")
+}
